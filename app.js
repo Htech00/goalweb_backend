e1c0543f7880f0df3c85d31b87b3c315d7befee8
Htech00 <htechsolutionz@gmail.com>
2025-07-28 13:04:02 +0100

    first commit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
+const cors = require("cors")
 
 const  goalRoutes = require("./routes/goalRoute");
 
